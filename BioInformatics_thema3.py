98e6f3ec70ddce155961038a874af5ef24ccb09f
Helen Polychroni <p20161@cslab.unipi.gr>
2023-07-22 13:45:23 +0300

    deal with the sequences

--- BioInformatics_thema3.py
+++ BioInformatics_thema3.py
@@ -3,11 +3,20 @@ Problem 6.14
 2 sequences (n and m length)
 '''
 
-
+''' Sequence 1'''
 # open file to read
 with open("sequence1.txt", "r") as file:
     # read sequence from file
     sequence1 = file.read().strip()
+    
+n = len(sequence1)
+''' Sequence 2'''
+# open file to read
+with open("sequence2.txt", "r") as file:
+    # read sequence from file
+    sequence2 = file.read().strip()
 
-# print sequence
-print(sequence1)
+m = len(sequence2)
+
+print("\nLength of 1st sequence is: (n) ",n)
+print("Length of 2nd sequence is: (m) ",m)
