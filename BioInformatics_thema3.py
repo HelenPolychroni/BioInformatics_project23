c245a6663e6eea9300b14e42ebb0f62331655227
Helen Polychroni <p20161@cslab.unipi.gr>
2023-07-22 13:34:16 +0300

    thema3

---
+++ BioInformatics_thema3.py
@@ -0,0 +1,13 @@
+'''
+Problem 6.14 
+2 sequences (n and m length)
+'''
+
+
+# open file to read
+with open("sequence1.txt", "r") as file:
+    # read sequence from file
+    sequence1 = file.read().strip()
+
+# print sequence
+print(sequence1)
